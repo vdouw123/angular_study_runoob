b423a173ac8b0ddfea213191a94a285f984728d3
张三丰 <123456@qq.com>
2017-05-07 21:29:23 +0800

    14 modules 模块

---
+++ 14_myApp.js
@@ -0,0 +1,5 @@
+/**
+ * Created by sf on 2017/5/7.
+ */
+
+var app = angular.module("myApp", []);
---
+++ 14_myCtrl.js
@@ -0,0 +1,8 @@
+/**
+ * Created by sf on 2017/5/7.
+ */
+
+app.controller("myCtrl", function($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+});
